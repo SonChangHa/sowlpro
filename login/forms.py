97e9a214062ae1b3e6d0f62ce65bc2f7732a80bc
son <jjang00198@gmail.com>
2020-02-01 19:12:36 +0900

    start

---
+++ login/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class LoginForm(ModelForm):  # 로그인을 제공하는 class.
+
+    class Meta:
+        model = User
+        widgets = {'password': forms.PasswordInput}
+        fields = ['username', 'password']
