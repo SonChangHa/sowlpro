bc40195a5f0007caf30b1640935cc5741876241a
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2020-02-12 18:27:38 +0900

    done

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # 인클루드는 ''이 포함된 url을 ()속에 매핑하라는 뜻, 즉 지금은 아무것도 안적혀있는 ~:8000/을 블로그점유알엘스로 매핑하는것.
     path('', include('login.urls')),
+    path('', include('point.urls')),
 ]
