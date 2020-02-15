bbf323d2851d01f43ec7fc03f4ba6ec75f58407d
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2020-02-16 01:21:04 +0900

    done

--- point/urls.py
+++ point/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('test', views.point_list, name='point'),
+   # path('test/mine', views.point_list_mine, name='mypoint'),
 
 ]
--- point/views.py
+++ point/views.py
@@ -2,25 +2,26 @@ from django.shortcuts import render
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def point_list(request):
-    scope = [
-        'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive',
-    ]
-    json_file_name = 'angelic-tracer-264105-2bd30234b387.json'
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-    gc = gspread.authorize(credentials)
-    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1c-X7io-Y5EahzJGG4wVOZO4xGXjTeCgIy2xSMeMDWDA/edit#gid=0'
-    # 스프레스시트 문서 가져오기
-    doc = gc.open_by_url(spreadsheet_url)
-    # 시트 선택하기
-    worksheet = doc.worksheet('시트1')
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+]
+json_file_name = 'angelic-tracer-264105-2bd30234b387.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1c-X7io-Y5EahzJGG4wVOZO4xGXjTeCgIy2xSMeMDWDA/edit#gid=0'
+# 스프레스시트 문서 가져오기
+doc = gc.open_by_url(spreadsheet_url)
+# 시트 선택하기
+worksheet = doc.worksheet('시트1')
 
-    # for i in range(1,3):
-        # row_data = worksheet.row_values(i)
-    # 범위(셀 위치 리스트) 가져오기
+# for i in range(1,3):
+# row_data = worksheet.row_values(i)
+# 범위(셀 위치 리스트) 가져오기
+
+
+def point_list(request):
     range_list = worksheet.range('A1:D180')
-    # 범위에서 각 셀 값 가져오기
 
     # 이제 여기서 html로 어떻게 값을 띄울것 인지 고민
     pointlist = []
@@ -38,4 +39,18 @@ def point_list(request):
 
     return render(request, 'point/point.html', {'point': pointlist})
 
-
+def point_list_mine(request):
+    range_list = worksheet.range('A1:D180')
+    pointlist = []
+    list = []
+    i = 0
+    for cell in range_list:
+        if cell.value == '':
+            break
+        list.append(cell.value)
+        i = i + 1
+        if i == 4:
+            if list[0] == user.first_name + user.last_name:
+                pointlist.append(list)
+            list = []
+            i = 0
