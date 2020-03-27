b94f3e00535b8157446734a6fefa517b812e7c7c
son <jjang00198@gmail.com>
2020-03-27 15:36:55 +0900

    start

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('table', views.table, name='table'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from point.views import throw_point
 
-
-
 def main(request):
     if request.user.is_active:
         mypoint = throw_point(request)
-        return render(request, 'main/index.html', {'mypoint': mypoint})
+        return render(request, 'main/main.html', {'mypoint': mypoint})
 
-    return render(request, 'main/index.html')
+    return render(request, 'main/main.html')
+
+def table(request):
+    if request.user.is_active:
+        mypoint = throw_point(request)
+        return render(request, 'main/table1.html', {'mypoint': mypoint})
+
+    return render(request, 'main/table1.html')
