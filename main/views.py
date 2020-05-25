1904f730eed0dd7df3245a6b083c2c7768d3493f
SonChangHa <jjang00198@gmail.com>
2020-05-26 06:54:28 +0900

    well

--- main/views.py
+++ main/views.py
@@ -5,6 +5,6 @@ def main(request):
     mypoint = throw_point(request)
     return render(request, 'main/main.html', {'mypoint': mypoint})
 
-def table(request):
+def free_table(request):
     mypoint = throw_point(request)
-    return render(request, 'main/table1.html', {'mypoint': mypoint})
+    return render(request, 'main/free_table.html', {'mypoint': mypoint})
